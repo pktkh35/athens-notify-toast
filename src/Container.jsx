47fac7c60820fafb5fe744e5dc377a8e75a8fc61
CodexCoding2000 <codexcoding99@gmail.com>
2023-10-29 19:15:50 +0700

    update

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,8 +1,6 @@
-import cx from 'clsx';
+import * as React from 'react';
 import { useToastContainer } from "./hooks/useContainer";
 import { Toast } from "./components/Toast";
-import { useCallback } from 'react';
-import { useStore } from './store/store';
 
 const Container = (props) => {
     const { getToastToRender, containerRef, updateHeightToast, calculateOffset } = useToastContainer({
@@ -13,22 +11,12 @@ const Container = (props) => {
         limit: 9
     });
 
-    function getClassName(position) {
-        const defaultClassName = cx(
-            `athens-container`,
-            `athens-${position}`
-        );
-
-        return cx(defaultClassName);
-    }
-
     return <div
         ref={containerRef}
     >
         {getToastToRender((position, toastList) => {
-
             return <div
-                className={getClassName(position)}
+                className={`athens-container athens-${position}`}
                 key={`container-${position}`}
             >
                 {toastList.map((t) => ({ ...t, offset: calculateOffset(t) })).map((toastProps, i) => {
