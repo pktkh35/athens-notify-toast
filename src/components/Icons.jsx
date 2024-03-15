e275df064612c4af836a5565c5e838aef4c0260d
CodexCoding2000 <codexcoding99@gmail.com>
2024-03-15 21:36:19 +0700

    fixing repeat variable

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import React, { cloneElement, isValidElement } from 'react';
+import { cloneElement, isValidElement } from 'react';
 import { isFn, isNum, isStr } from '../utils/propValidator';
 
 const Svg = ({ theme, type, children }) => (
